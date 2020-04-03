a6a24c06c14e6959235ddc0c981210ef08595522
mahmud009 <mahmud.est@gmail.com>
2020-04-03 21:52:54 +0600

    animation & preloader done

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
-$(window).on("load", function() {
+$(window).on("load", function () {
+  // Document ready function
+
   $("#preloader").fadeOut();
 
-  $(function() {
-    $(".nv-toggle").on("click", function() {
+  $(function () {
+    // Nav toggler class enable
+    $(".nv-toggle").on("click", function () {
       $(".navbar-cs-collapse").toggleClass("nav-opened");
       $("header").toggleClass("tg-open");
-
-      // if ($("header").hasClass("tg-open")) {
-      //   scrollLock.disablePageScroll();
-      // } else {
-      //   setTimeout(function() {
-      //     scrollLock.enablePageScroll();
-      //   }, 400);
-      // }
     });
   });
+
+  //Nicescroll initialize
+  $("body").niceScroll({
+    cursorcolor: "#fa7875",
+    cursoropacitymin: 0.5,
+    cursorborder: "none",
+    scrollspeed: 100,
+    mousescrollstep: 50,
+  });
+
+  //Scrolltop button fix
+  $(".scr-top").on("click", (e) => {
+    $("body").getNiceScroll(0).doScrollTop(0, 10);
+  });
+
+  //Scrolly js initialize
+  scrolly();
+
+  //AOS js initialize
+  AOS.init();
 });
